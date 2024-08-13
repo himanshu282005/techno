965aefc9dad8ce0b09c5586a96d5c145efc15487
himanshu282005 <himanshukalal909300@gmail.com>
2024-08-13 11:55:45 +0530

    js operators

---
+++ day7/index.js
@@ -0,0 +1,88 @@
+// console.log("hello techno");
+// // alert("danger");
+// // var,let,const
+// var firstnum =20;
+// console.log(firstnum); 
+//primitive types - string,number,boolean,undefined,symbol,big int
+//referance or object typed data types--array,objects,date,null
+
+// var firstvar="himanshu kalal"
+// console.log(firstvar)
+// console.log(typeof firstvar);
+
+// var firstvar="I' am a full stack 'developer'";
+// console.log(firstvar);
+// console.log(typeof firstvar);
+
+// var firstvar=false;
+// console.log(firstvar);
+// console.log(typeof firstvar);
+
+
+// var firstvar=undefined;
+// console.log(firstvar);
+// console.log(typeof firstvar);
+
+// var firstvar=[20,30,"hello",true,undefined];
+// console.log(firstvar);
+// console.log(typeof firstvar);
+
+// var firstvar=null;
+// console.log(firstvar);
+// console.log(typeof firstvar);
+// var firstvar={
+//     firstname :"himanshu",
+//     lastname:"kalal",
+
+// };
+// console.log(firstvar);
+// console.log(typeof firstvar);
+
+// var date=new Date();
+// console.log(date);
+// console.log(typeof date);
+
+// [{},{}.{}]-JSON
+
+//[[],[],[]]
+var num1=20;
+var num1=30;
+// variable defined with var can be  redecleard
+// let num2=20;
+// let num2=30;
+// variable defined with let can not be  redecleard but can be reassigned
+// variable defined with const can not be redecleard or reassigned
+// const num3=100;
+// num3=1000;
+
+
+//operators
+//arithmatic operators
+//+,-,*,/
+// let a=10;
+// let b=20;
+
+// let add=a+b;
+// console.log(firstname+""+lastname);
+// console.log(add)
+// console.log(a**b);
+// console.log(a%b);
+
+//type of NAN is a number
+console.log(5+"5");
+let y=10;
+// y=y-3;
+// y*=3;
+// y/=3;
+
+console.log(y);
+let num4=10;
+let num5=20;
+console.log(num4>num5);
+
+//logical operators
+let isloggedin=true;
+let isadmin=true;
+console.log("is the user logged in and an admin?",isloggedin && isadmin);
+
+
